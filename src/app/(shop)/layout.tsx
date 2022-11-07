b7e9af694c6c56fbb25efa6f15dadbaf696f51eb
Alexis Barba <alexbarba.2278@gmail.com>
2022-11-06 23:31:03 -0700

    feat(header): add elements (logo, search input, cart button)

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -6,10 +6,10 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <section className="flex min-h-screen flex-col justify-between bg-slate-800 text-slate-100">
       <Header />
-      {children}
+      <section className="h-full p-4">{children}</section>
       <Footer />
-    </>
+    </section>
   );
 }
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,32 @@
+"use client";
+import Image from "next/image";
+import Logo from "../../../../public/logo.webp";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="grid-4 flex h-16 items-center justify-between bg-slate-600 p-4 shadow-md sm:px-8">
+      <Image
+        src={Logo}
+        alt="Logo"
+        className="aspect-square w-10 rounded-full bg-white object-cover p-1"
+      />
+
+      <label className="align-center relative flex h-full w-full max-w-md justify-center">
+        <input
+          type="text"
+          placeholder="Buscar producto..."
+          className="h-full w-full rounded-md border border-slate-100 bg-slate-700 pl-10 focus:ring-1 focus:ring-slate-50 focus-visible:outline-none"
+        />
+        <MagnifyingGlassIcon className="absolute inset-y-0 left-2 my-auto h-4 w-4" />
+      </label>
+
+      <button className="p-2">
+        <ShoppingBagIcon className="h-6 w-6" />
+      </button>
+    </header>
+  );
 };
 
 export default Header;
