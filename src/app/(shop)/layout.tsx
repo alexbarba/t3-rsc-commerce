fdb467edb787b7e511ba8ec8e600ea3551b8a17e
Alexis Barba <alexbarba.2278@gmail.com>
2022-11-06 22:40:38 -0700

    add header and footer to shop layout

---
+++ src/app/(shop)/layout.tsx
@@ -0,0 +1,15 @@
+import { Header, Footer } from "@components/common";
+
+export default function ShopLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
