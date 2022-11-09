ff29c49bd5eb4c2b769b71f75ca6d39e5edd9a20
Alexis Barba <alexbarba.2278@gmail.com>
2022-11-08 18:09:33 -0600

    refactor(shop-layout)

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -6,9 +6,9 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex min-h-screen flex-col justify-between bg-slate-800 text-slate-100">
+    <section className="flex min-h-screen flex-col bg-slate-800 text-slate-100">
       <Header />
-      <section className="h-full justify-self-start p-4">{children}</section>
+      <section className="h-full grow p-4">{children}</section>
       <Footer />
     </section>
   );
