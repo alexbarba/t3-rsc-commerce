898036f4955658cfb810f832ddfdfe831630f2a5
Alexis Barba <alexbarba.2278@gmail.com>
2022-11-10 17:10:57 -0600

    refactor(ProductsTable): refactor styles and remove edit icon

--- src/components/admin/ProductsTable/ProductsTable.tsx
+++ src/components/admin/ProductsTable/ProductsTable.tsx
@@ -1,15 +1,14 @@
 import type { FC } from "react";
 import type { Product } from "@prisma/client";
 import Link from "next/link";
-import { PencilSquareIcon } from "@heroicons/react/24/solid";
 
 const thClassnames = "font-medium p-4 pr-8 pt-0 pb-3 ";
-const tdClassnames = "p-4 truncate";
+const tdClassnames = "px-4 py-8 truncate";
 
 const ProductsTable: FC<{ products: Product[] }> = ({ products }) => {
   return (
-    <div className="rounded-xl border border-slate-600 bg-slate-600 p-4">
-      <table className="h-full w-full table-auto overflow-auto ">
+    <div className="w-full max-w-[calc(100vw-200px)] overflow-auto rounded-xl border border-slate-600 bg-slate-600 p-4">
+      <table className="h-full w-full table-auto  ">
         <thead>
           <tr className="border-b border-slate-400 text-center">
             <th className={thClassnames}>Id</th>
@@ -17,9 +16,8 @@ const ProductsTable: FC<{ products: Product[] }> = ({ products }) => {
             <th className={thClassnames}>Descripción</th>
             <th className={thClassnames}>Precio</th>
             <th className={thClassnames}>Stock</th>
-            <th className={thClassnames}>Actualizado</th>
+            <th className={thClassnames}>Última actualización</th>
             <th className={thClassnames}>Creado</th>
-            <th className={thClassnames}>Editar</th>
           </tr>
         </thead>
         <tbody>
@@ -29,7 +27,14 @@ const ProductsTable: FC<{ products: Product[] }> = ({ products }) => {
               className="border-b border-slate-400 text-center"
             >
               <td className={tdClassnames}>{product.id}</td>
-              <td className={tdClassnames}>{product.name}</td>
+              <td className={tdClassnames}>
+                <Link
+                  href={`/products/${product.id}`}
+                  className="font-bold text-emerald-300"
+                >
+                  {product.name}
+                </Link>
+              </td>
               <td className={tdClassnames}>{product.description}</td>
               <td className={tdClassnames}>
                 {" "}
@@ -44,12 +49,6 @@ const ProductsTable: FC<{ products: Product[] }> = ({ products }) => {
               </td>
               <td className={tdClassnames}>
                 {new Date(product.createdAt).toLocaleString()}
-              </td>
-
-              <td className={tdClassnames}>
-                <Link href={`/products/${product.id}/edit`}>
-                  <PencilSquareIcon className="mx-auto h-6 w-6" />
-                </Link>
               </td>
             </tr>
           ))}
